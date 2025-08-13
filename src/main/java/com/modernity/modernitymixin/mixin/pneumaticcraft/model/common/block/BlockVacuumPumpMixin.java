d8f7c2b4d35057dc8d0ed5ce90c6e1170be8f4fc
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-08-13 13:03:07 +0800

    Update BlockVacuumPumpMixin.java

--- src/main/java/com/modernity/modernitymixin/mixin/pneumaticcraft/model/common/block/BlockVacuumPumpMixin.java
+++ src/main/java/com/modernity/modernitymixin/mixin/pneumaticcraft/model/common/block/BlockVacuumPumpMixin.java
@@ -3,12 +3,13 @@ package com.modernity.modernitymixin.mixin.pneumaticcraft.model.common.block;
 import me.desht.pneumaticcraft.common.block.BlockVacuumPump;
 import net.minecraft.util.BlockRenderLayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(value = BlockVacuumPump.class, remap = false)
 public class BlockVacuumPumpMixin {
 
-    // Set the render layer translucent. Do not add @Unique.
-    @SuppressWarnings({"unused", "MissingUnique", "AddedMixinMembersNamePattern"})
+    @Unique
+    @SuppressWarnings({"unused", "AddedMixinMembersNamePattern"})
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.TRANSLUCENT;
     }
