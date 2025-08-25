d2018328ee1d6fe4030c484da8245d6f0971a4f3
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2025-08-25 14:27:22 +0800

    Fix import

--- src/main/java/com/modernity/modernitymixin/mixin/vanilla/GuiTextFieldMixin.java
+++ src/main/java/com/modernity/modernitymixin/mixin/vanilla/GuiTextFieldMixin.java
@@ -2,6 +2,8 @@ package com.modernity.modernitymixin.mixin.vanilla;
 
 import net.minecraft.client.gui.GuiTextField;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(GuiTextField.class)
 public class GuiTextFieldMixin {
